05df1c5fd07f72095d259c56149a0872c8e971bf
heyward <sin55123456@gmail.com>
2025-02-09 01:53:20 +0800

    Finetune qwen2.5-0.5b-instruct on s1-1k.

--- train/sft.sh
+++ train/sft.sh
@@ -1,13 +1,23 @@
+export CUDA_VISIBLE_DEVICES=0,1
 uid="$(date +%Y%m%d_%H%M%S)"
-base_model="Qwen/Qwen2.5-32B-Instruct"
+# base_model="Qwen/Qwen2.5-32B-Instruct"
+# micro_batch_size=2 # -> batch_size will be 16 if 8 gpus
+# push_to_hub=true
+# gradient_accumulation_steps=2
+
+base_model="Qwen/Qwen2.5-0.5B-Instruct"
+# base_model="Qwen/Qwen2.5-1.5B-Instruct"
+train_dataset="simplescaling/s1K_tokenized"
+# train_dataset="bespokelabs/Bespoke-Stratos-17k"
 lr=1e-5
 min_lr=0
-epochs=5
-micro_batch_size=2 # -> batch_size will be 16 if 8 gpus
+epochs=10
+micro_batch_size=1 # -> batch_size will be 16 if 8 gpus
 push_to_hub=true
-gradient_accumulation_steps=2
+gradient_accumulation_steps=4
 max_steps=-1
-gpu_count=$(nvidia-smi -L | wc -l)
+# gpu_count=$(nvidia-smi -L | wc -l)
+gpu_count=2
 
 torchrun --nproc-per-node ${gpu_count} --master_port 12345 \
 train/sft.py \
@@ -16,7 +26,7 @@ train/sft.py \
 --gradient_accumulation_steps=${gradient_accumulation_steps} \
 --num_train_epochs=${epochs} \
 --max_steps=${max_steps} \
---train_file_path="simplescaling/s1K_tokenized" \
+--train_file_path=${train_dataset} \
 --model_name=${base_model} \
 --warmup_ratio=0.05 \
 --fsdp="full_shard auto_wrap" \
@@ -32,6 +42,9 @@ train/sft.py \
 --adam_beta1=0.9 \
 --adam_beta2=0.95 \
 --output_dir="ckpts/s1_${uid}" \
---hub_model_id="simplescaling/s1-${uid}" \
+--hub_model_id="heyward/s1-${uid}" \
 --push_to_hub=${push_to_hub} \
---save_only_model=True
+--save_only_model=True \
+--wandb_project="s1-qwen2.5-0.5b" \
+--wandb_entity="nycu_cadlab" \
+--use_liger 
